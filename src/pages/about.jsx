557b8f7a739c6b7104d084498ae8c9e0493a8223
Davi Souza Santos <davii.clink@gmail.com>
2024-03-15 21:01:02 -0300

    criação e parametrização do projeto

---
+++ src/pages/about.jsx
@@ -0,0 +1,7 @@
+export default function about() {
+    return (
+        <main className="w-screen bg-botoes h-screen">
+            teste
+        </main>
+    )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,11 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+
+      colors: {
+        'fundo': '#18181B',
+        'botoes': '#8B5CF64D'
+      },
     },
   },
   plugins: [],
